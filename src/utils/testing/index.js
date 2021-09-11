d2527d12fd2193d59bb1e704559de9007bfd7f15
Abhipriyo Biswas <biswasabhi93@gmail.com>
2021-09-11 13:08:04 +0530

    Removed useless files

--- src/utils/testing/index.js
+++ src/utils/testing/index.js
@@ -1,3 +1,4 @@
+// To find component by data test attribute
 export const findByTestAttr = (component, attribute) => {
   const wrapper = component.find(`[data-test='${attribute}']`);
   return wrapper;
