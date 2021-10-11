3523f2f773093efc3c3dce3a25b5e7e1c2773e2f
Abhipriyo Biswas <abhipriyo.biswas@etelligens.in>
2021-10-11 17:00:48 +0530

    Backend: packages installed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const cors = require("cors");
+const env = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 const schema = require("./schema/index");
+const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 
+// Environment Config
+env.config();
+
+// Express App
 const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Cross-Origin Connection Handler
 app.use(cors());
@@ -30,6 +40,6 @@ app.use(
 );
 
 // Listening to Server
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server up and running");
 });
