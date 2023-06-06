a1493c37ba89105882898b9ce27ae0cd698a79be
Abhipriyo Biswas <biswasabhi93@gmail.com>
2023-06-06 16:28:41 +0530

    FrontEnd: separated client from server

--- backend/models/intro.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schema = mongoose.Schema;
-
-const introSchema = new schema({
-  name: String,
-  specialization: Array,
-});
-
-module.exports = mongoose.model("Intro", introSchema);
--- backend/models/skills.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const skillsSchema = new Schema({
-	skill: String,
-	proficiency: Number,
-});
-
-module.exports = mongoose.model("Skills", skillsSchema);
